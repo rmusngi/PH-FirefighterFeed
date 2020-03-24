f6d37f8f21f963dd421a2806dc47389ee4901035
Rudolph Musngi <rudolphmusngi@gmail.com>
2020-03-24 18:46:31 +0800

    initial commit

---
+++ js/loader.js
@@ -0,0 +1,16 @@
+$(function () {
+    $(".brainly-questions").slice(0, 5).show();
+    $("#loadMore").on('click', function (e) {
+        e.preventDefault();
+        $(".brainly-questions:hidden").slice(0, 5).slideDown();
+        if ($(".brainly-questions:hidden").length == 0) {
+            $("#load").fadeOut('slow');
+        }
+        $('html,body').animate({
+            scrollTop: $(this).offset().top
+        }, 1500);
+    });
+});
+
+
+
